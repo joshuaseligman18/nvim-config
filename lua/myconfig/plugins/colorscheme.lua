f5c370a92274dfb818565cef6612c87ad5feb98d
Josh Seligman <joshuaseligman18@gmail.com>
2024-02-22 11:04:29 -0500

    Fixed a typo

--- lua/myconfig/plugins/colorscheme.lua
+++ lua/myconfig/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
     priority = 1000,
     config = function()
         require('rose-pine').setup({
-            disable_backgraund = true
+            disable_background = true,
         })
         vim.cmd.colorscheme("rose-pine")
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
