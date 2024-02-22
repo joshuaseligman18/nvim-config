468924af72857659c754dfdff35756e8c405d6e2
Josh Seligman <joshuaseligman18@gmail.com>
2024-02-22 10:27:58 -0500

    Removed background color from the nvim config

--- lua/myconfig/plugins/colorscheme.lua
+++ lua/myconfig/plugins/colorscheme.lua
@@ -3,6 +3,11 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd("colorscheme rose-pine")
+        require('rose-pine').setup({
+            disable_backgraund = true
+        })
+        vim.cmd.colorscheme("rose-pine")
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end,
 }
